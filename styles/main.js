415f7561aac24f3082abca5323fee0b3f1ac23d5
eminem2212 <ibrahimimran131@gmail.com>
2022-03-17 17:31:08 -0400

    om

---
+++ styles/main.js
@@ -0,0 +1,106 @@
+/* var carName = "";
+demo.innerHTML = carName; */
+
+
+
+
+/* var Username = prompt("Enter your name:")
+demo.innerHTML = Username;
+var UserAge = prompt("Enter your Age: ")
+demola.innerHTML = UserAge;
+var UserAddress = prompt("Enter your Address: ")
+sikiru.innerHTML = UserAddress;
+var UserPhone = prompt("Enter your Phone number: ")
+hackNasa.innerHTML = UserPhone;
+ */
+
+
+/* alert("welcome to JavaScript " + Username) */
+
+/* let sec=60;
+let hour= 60*sec;
+let day=24;
+let o= 24 * hour;
+let week = o * 7
+console.log(week)
+weeks.innerHTML =week
+ */
+
+
+
+/*let tip = 30/100
+let amount = 10000
+let people = 3
+let tipamount = tip * amount
+let actualamount = tipamount + amount
+let amountpayable = actualamount/people
+alert("welcome to Dominos page")
+tip= prompt("Enter the tip amount")
+amount =prompt('Enter actual amount')
+alert("thank you for the tip")
+console.log(prize)
+prize.innerHTML =amountpayable*/
+
+alert('welcome to calculator');
+let amount = prompt ('enter the amount');
+let percent = prompt ('enter the percentage');
+let friends = prompt ('enter number of friends');
+
+let prize = parseInt (amount);
+let percentage = parseInt(percent);
+let share = parseInt(friends);
+
+let total = prize + (percentage/100 * prize)
+let grandtotal = total/share
+
+
+console.log(grandtotal);
+emii.innerHTML = grandtotal;
+
+
+
+
+
+
+/*alert("Welcome to My dashboard,Kindly fill the below to know your discount")
+amount = prompt('Enter actual amount')
+
+const data = {
+    "amount": amount
+}
+
+
+if (data["amount"] < 2000) {
+    alert("You are eligible for 10% discount")
+    console.log(prize)
+    prize.innerHTML = amount
+
+
+}
+else if ((data["amount"] >= 2000) && (data["amount"] <= 5000)) {
+    alert("You are eligible for 20% discount")
+    console.log(prize)
+    prize.innerHTML = amount
+
+
+}
+else if (data["amount"] > 5000) {
+    alert("You are eligible for 30% discount")
+    console.log(prize)
+    prize.innerHTML = amount
+
+
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
